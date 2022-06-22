a8330fa372c11f9e4efba91bb169d5d9b794fc5b
jroiceee <jroicealdeza@gmail.com>
2022-06-22 15:07:51 +0800

    add table

---
+++ events.js
@@ -0,0 +1,6 @@
+function buttonEvents() {
+    const drawButton = document.querySelector("#draw-button");
+    drawButton.addEventListener('click', (e) => location.href=`https://roiceee.github.io/PixelPaint`);
+}
+
+buttonEvents();
